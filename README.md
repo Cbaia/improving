5ae20ec480e875ddb90f79220948b4f44ff429b4
Cbaia <120639579+Cbaia@users.noreply.github.com>
2025-12-29 19:30:38 +0000

    adiciona scripts 'lint:check'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # improving
+
 Criando algumas coisas
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-function Home(){
-    return <h1>Validação concluída, Parabens!!!. Daremos seguimento para a segunda etapa</h1>
-    
+function Home() {
+  return (
+    <h1>
+      Validação concluída, Parabens!!!. Daremos seguimento para a segunda etapa
+    </h1>
+  );
+
+  function teste() {
+    console.log("teste");
+  }
+  function teste2() {
+    console.log("Identação errada");
+  }
 }
 
 export default Home;
